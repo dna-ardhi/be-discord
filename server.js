19b8380c8da49be350667ad705553787d9c391dd
Ardhi <dadinurardhi@gmail.com>
2022-06-08 09:43:48 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
+
+const port = process.env.PORT || process.env.API_PORT;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Register the routes
+app.use('/api/auth/', authRoutes);
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(port, () => {
+      console.log(`server is listening port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log('database connection failed. Server not started');
+    console.error(error);
+  });
