07679457266143e4c1098b66d653a07e15d1a7d9
Ardhi <dadinurardhi@gmail.com>
2022-07-02 13:38:46 +0700

    feat: add auth controllers

---
+++ controllers/auth/postLogin.js
@@ -0,0 +1,39 @@
+const User = require('../../models/users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const postLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // send new token
+      const token = jwt.sign(
+        {
+          userId: user._id,
+          email,
+        },
+        process.env.JWT_KEY,
+        {
+          expiresIn: '24h',
+        }
+      );
+      const response = {
+        userDetails: {
+          email: user.email,
+          username: user.username,
+          token,
+        },
+      };
+      return res.status(200).json(response);
+    }
+
+    return res.status(400).send('Invalid credential. Please try again');
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('Something went wrong. Please try again');
+  }
+};
+
+module.exports = postLogin;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../controllers/auth');
+const Joi = require('joi');
+const validator = require('express-joi-validation').createValidator({});
+const authMiddleware = require('../middleware/auth');
 
-router.post('/register', (req, res) => {
-  res.send('register route');
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(12).required(),
+  password: Joi.string().min(6).max(12).required(),
+  email: Joi.string().email().required(),
 });
 
-router.post('/login', (req, res) => {
-  res.send('login route');
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).max(12).required(),
+  email: Joi.string().email().required(),
+});
+
+router.post(
+  '/register',
+  validator.body(registerSchema),
+  auth.controllers.postRegister
+);
+
+router.post('/login', validator.body(loginSchema), auth.controllers.postLogin);
+
+// test router
+router.get('/test', authMiddleware, (req, res) => {
+  res.send('request passed');
 });
 
 module.exports = router;
