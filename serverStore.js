8fda777abaa3497f8ada35b0c641fed833bd550d
Ardhi <dadinurardhi@gmail.com>
2022-07-19 18:07:22 +0700

    feat: create disconnect handler

---
+++ serverStore.js
@@ -0,0 +1,20 @@
+const connectedUser = new Map();
+
+const addNewConnectedUser = ({ socketId, userId }) => {
+  connectedUser.set(socketId, { userId });
+  console.log(`new connected users : `);
+  console.log(connectedUser);
+};
+
+const removeConnectedUser = (socketId) => {
+  if (connectedUser.has(socketId)) {
+    connectedUser.delete(socketId);
+    console.log('remove connected users :');
+    console.log(connectedUser);
+  }
+};
+
+module.exports = {
+  addNewConnectedUser,
+  removeConnectedUser,
+};
