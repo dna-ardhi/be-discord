d9e22bdc1bdcc1ebe07d97965e132492036aade6
Ardhi <dadinurardhi@gmail.com>
2022-07-19 22:02:21 +0700

    feat: create controller friend Invitation

--- controllers/friends/postInvite.js
+++ controllers/friends/postInvite.js
@@ -1,6 +1,57 @@
+const User = require('../../models/users');
+const Invitation = require('../../models/friendInvitation');
+
 const postInvite = async (req, res) => {
   const { targetEmail } = req.body;
-  return res.send('controller is working!');
+  const { userId, email } = req.user;
+
+  if (email.toLowerCase() === targetEmail.toLowerCase()) {
+    return res
+      .status(409)
+      .send('Sorry. you cannot become friend with yourself');
+  }
+
+  const targetUser = await User.findOne({
+    email: targetEmail.toLowerCase(),
+  });
+
+  if (!targetUser) {
+    return res
+      .status(404)
+      .send(
+        `Friend of ${targetEmail} has not been found. Please check email address.`
+      );
+  }
+
+  //  check if invitation has been already sent
+
+  const invitationAlreadyReceive = await Invitation.findOne({
+    senderId: userId,
+    receiverId: targetUser._id,
+  });
+
+  if (invitationAlreadyReceive) {
+    return res.status(409).send('Invitation has been already sent!');
+  }
+
+  // check if the user which we would like to invite is already our friend
+  const userAlreadyFriends = targetUser.friends.find(
+    (friendId) => friendId.toString() === userId.toString()
+  );
+
+  if (userAlreadyFriends) {
+    return res
+      .status(409)
+      .send('Friend already added. Please check friends list');
+  }
+
+  // create new invitation in database
+  const newInvitation = await Invitation.create({
+    senderId: userId,
+    receiverId: targetUser._id,
+  });
+
+  return res.status(201).send('Invitation has been sent!');
 };
 
 module.exports = postInvite;
